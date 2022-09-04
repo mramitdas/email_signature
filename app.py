88bc5aeed196c385e6c1900cd3417dfca2ddf841
amit <mramitdas99@gmail.com>
2022-09-04 12:57:19 +0530

    added: api

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,16 @@ app = Flask(__name__)
 @app.route('/', methods=["GET", "POST"])
 def home():
     return render_template('sign.html')
+
+
+@app.route('/api', methods=["GET", "POST"])
+def api():
+    if request.method == 'POST':
+        data = request.get_json()
+
+        if {'name', 'position', 'phone', 'email', 'location'}.issubset(data.keys()):
+            return "xyz"
+    return ""
 
 
 if __name__ == '__main__':
